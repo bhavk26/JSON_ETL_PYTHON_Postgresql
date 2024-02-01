df75e29c0cd353e43c8a768c3085f337b3d9991e
bhavk26 <123792424+bhavk26@users.noreply.github.com>
2024-02-01 19:08:51 +0530

    Initiating logger

--- logs.py
+++ logs.py
@@ -1,6 +1,6 @@
 import logging
 
-
+#initiating logger class
 def init_logger():
     logging.basicConfig(level=logging.INFO, format='%(asctime)s-%(levelname)s-%(message)s')
     logging.info('Starting logger')
