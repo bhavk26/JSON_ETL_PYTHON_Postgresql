35b25b96fced845d4c4dfcb69d7989594323bad6
bhavk26 <123792424+bhavk26@users.noreply.github.com>
2024-02-01 19:10:42 +0530

    Reading json from source

--- readjson.py
+++ readjson.py
@@ -1,6 +1,6 @@
 import requests as r
 import json
-
+#reading json from source
 def read():
     g = r.get(
         'https://api.data.gov.in/resource/2297dfd8-2ba7-49a6-b53f-8796568d4753?api-key=579b464db66ec23bdd000001cdd3946e44ce4aad7209ff7b23ac571b&format=json')
